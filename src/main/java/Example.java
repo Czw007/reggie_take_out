a3a39fcbef0393620716d2bb61afe9e14c6277df
ZhiWei Chang <2778571381@qq.com>
2023-10-18 16:34:53 +0800

    新增禁用员工账号、编辑账号

---
+++ src/main/java/Example.java
@@ -0,0 +1,12 @@
+public class Example {
+//    String str = new String("good");
+    String str="good";
+    char[] ch = { 'a', 'b', 'c' };
+    public static void main(String args[]) {
+        Integer x=new Integer(123);
+        Integer y=new Integer(123);
+        System.out.println(x);
+        System.out.println(x==y);
+    }
+
+}
