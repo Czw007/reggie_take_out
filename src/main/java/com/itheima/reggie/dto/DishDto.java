e7e4114f3b62ed3d6de4ceda901710308fa700a7
ZhiWei Chang <2778571381@qq.com>
2023-10-26 18:19:59 +0800

    新增菜品、菜品信息分页查询、修改菜品

---
+++ src/main/java/com/itheima/reggie/dto/DishDto.java
@@ -0,0 +1,20 @@
+package com.itheima.reggie.dto;
+
+import com.itheima.reggie.entity.Dish;
+import com.itheima.reggie.entity.DishFlavor;
+import lombok.Data;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * DTO 数据参数对象，一般用于展示层和服务层之间的数据传输，前端数据和实体类对不上的时候需要重新封装这么一个类当作数据传输对象
+ */
+@Data
+public class DishDto extends Dish {
+
+    private List<DishFlavor> flavors = new ArrayList<>();
+
+    private String categoryName;
+
+    private Integer copies;
+}
